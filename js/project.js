1f5267da919b9fc320f9aeb310e8c453b4f09610
shldecke <shldecke@ucsc.edu>
2023-05-23 12:04:34 -0700

    did some basics for the website to get everyone started

---
+++ js/project.js
@@ -0,0 +1,5 @@
+// project.js - The JavaScript File for Our Group Project
+// Author: Shelby Decker ADD NAMES HERE
+// Due: 5/30/23
+
+// this will be the javascript file that does 'stuff' on our webpage
