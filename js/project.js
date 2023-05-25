73103716a47d1ac39e4a9517f67b3745981e2b66
shldecke <shldecke@ucsc.edu>
2023-05-25 10:31:41 -0700

    js update-- small

--- js/project.js
+++ js/project.js
@@ -3,3 +3,20 @@
 // Due: 5/30/23
 
 // this will be the javascript file that does 'stuff' on our webpage
+// using async function so other functions of the website will be usable
+async function random_lofi(){
+    // declare the request URL from github site
+    const requestURL = "https://github.io/shldecke/Art101Group10Project/js/lofi.json";
+    // initialize a new request object
+    const request = new Request(requestURL);
+
+    // make a network request using fetch() and returns a response obj
+    const response = await fetch(request);
+    // retreive the response as a JSON using .json
+    const lofi_songs = await response.json();
+
+    // randomize & then store in a variable
+
+    // once we've selected the song, it's time to append to the webpage
+
+}
